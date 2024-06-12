e7b97db8d7da89fc7f697cc69ff6ad978543f9ca
amitonline <sengupta_amit@hotmail.com>
2024-06-12 16:32:26 +0530

    Fixing projects to run in VS 2022 Windows 10

--- abs-pos/main.cpp
+++ abs-pos/main.cpp
@@ -1,7 +1,11 @@
 #include "main.h"
 #include "abs-pos.h"
 
-IMPLEMENT_APP(MainApp)
+#ifndef _WIN32
+IMPLEMENT_APP(MainApp);
+#else
+wxIMPLEMENT_APP(MainApp);
+#endif 
 
 bool MainApp::OnInit() {
     Absolute* abs = new Absolute(wxT("Absolute Positioning"));
