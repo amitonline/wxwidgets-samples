3ab7cfc7c6a8ad19551772ef717a5c561163a389
amitonline <sengupta_amit@hotmail.com>
2024-06-14 17:08:32 +0530

    Making samples compatible with VS under Windows

--- archive-formats/main.cpp
+++ archive-formats/main.cpp
@@ -1,7 +1,13 @@
 #include "main.h"
 #include "archive.h"
 
-IMPLEMENT_APP(MainApp)
+
+#ifndef _WIN32
+IMPLEMENT_APP(MainApp);
+#else
+wxIMPLEMENT_APP(MainApp);
+#endif 
+
 
 bool MainApp::OnInit() {
     Archive *arc = new Archive(wxT("Archive demo"));
